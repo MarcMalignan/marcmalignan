10fc33b43e4bd1060a4011f350c72b103eb5d499
MarcMalignan <mhmalignan@gmail.com>
2019-12-22 12:59:35 +0100

    feat: default language value

--- src/services/language/language.tsx
+++ src/services/language/language.tsx
@@ -8,11 +8,21 @@ export enum LANGUAGES {
   FR = 'fr',
 }
 
+const getUserLanguage = () => {
+  if (window.navigator && window.navigator.language) {
+    return window.navigator.language.includes(LANGUAGES.FR)
+      ? LANGUAGES.FR
+      : LANGUAGES.EN;
+  }
+
+  return LANGUAGES.FR;
+};
+
 export const initLanguage = () =>
   i18next.use(initReactI18next).init({
     resources: {
       en: enTranslations,
       fr: frTranslations,
     },
-    lng: LANGUAGES.FR,
+    lng: getUserLanguage(),
   });
